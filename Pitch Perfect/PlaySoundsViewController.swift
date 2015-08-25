b924128a2ca0b3ad1281e050ea0866ccd898cd89
Edward Groberski <egroberski@gmail.com>
2015-08-24 22:51:09 -0500

    Implemented slow & fast sound playback

---
+++ Pitch Perfect/PlaySoundsViewController.swift
@@ -0,0 +1,70 @@
+//
+//  PlaySoundsViewController.swift
+//  Pitch Perfect
+//
+//  Created by Eddie Groberski on 8/24/15.
+//  Copyright (c) 2015 Eddie Groberski. All rights reserved.
+//
+
+import UIKit
+import AVFoundation
+
+class PlaySoundsViewController: UIViewController {
+    let slowRate: Float = 0.5
+    let fastRate: Float = 1.5
+    var audioPlayer = AVAudioPlayer()
+    @IBOutlet weak var stopButton: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        stopButton.hidden = true
+        if var soundPath = NSURL(fileURLWithPath: NSBundle.mainBundle().pathForResource("movie_quote", ofType: "mp3")!) {
+            var error:NSError?
+            audioPlayer = AVAudioPlayer(contentsOfURL:soundPath, error:&error)
+            audioPlayer.enableRate = true
+            audioPlayer.prepareToPlay()
+        } else {
+            println("Could not find audio file path")
+        }
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func slowAudio(sender: UIButton) {
+        changeAudioRate(slowRate)
+    }
+
+    @IBAction func fastAudio(sender: UIButton) {
+        changeAudioRate(fastRate)
+    }
+    
+    func changeAudioRate(rate: Float) {
+        audioPlayer.stop()
+        audioPlayer.currentTime = NSTimeInterval(0)
+        audioPlayer.rate = rate
+        audioPlayer.play()
+        stopButton.hidden = false
+    }
+    
+    @IBAction func stopAudio(sender: UIButton) {
+        audioPlayer.stop()
+        audioPlayer.currentTime = NSTimeInterval(0)
+        stopButton.hidden = true
+    }
+    
+    
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
