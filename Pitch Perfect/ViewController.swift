7fd988fe93602630b86a3f2357d1b9bcf7122ce6
Edward Groberski <egroberski@gmail.com>
2015-08-24 21:33:46 -0500

    Hide / show button actions

--- Pitch Perfect/ViewController.swift
+++ Pitch Perfect/ViewController.swift
@@ -11,23 +11,28 @@ import UIKit
 class ViewController: UIViewController {
 
     @IBOutlet weak var recordingInProgress: UILabel!
+    @IBOutlet weak var stopButton: UIButton!
+    @IBOutlet weak var recordButton: UIButton!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    override func viewWillAppear(animated: Bool) {
+        recordButton.enabled = true
+        stopButton.hidden = true
     }
-
 
     @IBAction func recordAudio(sender: UIButton) {
         recordingInProgress.hidden = false
+        recordButton.enabled = false
+        stopButton.hidden = false
     }
     @IBAction func stopRecording(sender: UIButton) {
         recordingInProgress.hidden = true
+        recordButton.enabled = true
+        stopButton.hidden = true
     }
 }
 
